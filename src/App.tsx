45d48a5b040f2de6d6f4ae0b1ea66e67c1ce248c
kevsbry <nivekbryan09.projects@gmail.com>
2022-01-18 23:09:25 +0800

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<LoginPage />} />
       </Routes>
